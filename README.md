ca5fbd19a41eff7c923614339e7b238a858f0fa5
bernes1 <37117219+bernes1@users.noreply.github.com>
2026-05-16 16:46:00 +0200

    add .gitignore and update README; implement network connection using secrets

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # this-is-fine
 this is fine
+
+add file named secrets.py with credentials
+like this 
+
+```
+SSID="<insert name of wifi network>"
+PASSWORD="<insert WIFI password>"
+```
+
+
+how to use
+```
+echo -n "activate" | nc -u <device-ip> 5005
+``` 
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import network
+import socket
+import secrets
 from machine import Pin
 from utime import sleep
 
@@ -11,10 +14,22 @@ def activate():
     sleep(0.1)
     pin.off()
 
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(secrets.SSID, secrets.PASSWORD)
+    while not wlan.isconnected():
+        sleep(0.5)
+    print("IP:", wlan.ifconfig()[0])
 
-activate()
+def serve():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(("0.0.0.0", 5005))
+    while True:
+        data, _ = s.recvfrom(64)
+        if data == b"activate":
+            print("recived")
+            activate()
 
-
-
-
-
+connect_wifi()
+serve()
